a92184b36fb5bee13fe35619a1cce1010b9fb402
Tenzin Sonam <tenso2006@gmail.com>
2017-04-25 23:07:55 -0700

    Add: getMaxProfit in linear time

---
+++ getMaxProfit.js
@@ -0,0 +1,14 @@
+(function (){
+  var getMaxProfit = function (arr) {
+    var minVal = arr[0];
+    var maxProfit = 0;
+    arr.forEach(function (val, i) {
+      if(val < minVal) {
+        minVal = val;
+      }
+      maxProfit = Math.max(maxProfit, val - minVal);
+    });
+    return maxProfit;
+  };
+  console.log(getMaxProfit([10, 7, 5, 8, 11, 9]));
+})();
