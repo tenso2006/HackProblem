79ea51fe99f519485b5d77ffd741a1d8246af1be
Tenzin Sonam <tenso2006@gmail.com>
2017-05-15 19:25:17 -0700

    add: linkedList structure

---
+++ LinkedList.js
@@ -0,0 +1,27 @@
+var LinkedList = function () {
+  this.head = null;
+};
+
+LinkedList.prototype.isEmpty = function () {
+
+};
+
+LinkedList.prototype.size = function () {
+
+};
+
+LinkedList.prototype.contains = function () {
+
+};
+
+LinkedList.prototype.prepend = function () {
+
+};
+
+LinkedList.prototype.append = function () {
+
+};
+
+LinkedList.prototype.remove = function () {
+
+};
