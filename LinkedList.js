89831539f0d68aa21c3cd16950008b75b06d0694
Tenzin Sonam <tenso2006@gmail.com>
2017-05-15 23:56:25 -0700

    Add: size method

--- LinkedList.js
+++ LinkedList.js
@@ -6,12 +6,22 @@ LinkedList.prototype.isEmpty = function () {
   return this.head === null;
 };
 
+
+// [10] -> [20] -> [30] -> null
+LinkedList.prototype.size = function () {
+  var count = 0;
+  var current = this.head;
+
+  while(current !== null) {
+    count++;
+    current = current.next;
+  }
+  return count;
+};
+
 var list = new LinkedList();
 console.log(list.isEmpty());
-
-LinkedList.prototype.size = function () {
-
-};
+console.log(list.size());
 
 LinkedList.prototype.contains = function () {
 
