708956232f0ae8059f6bdafdd6b7e03bbdfd1842
Tenzin Sonam <tenso2006@gmail.com>
2017-06-06 23:27:19 -0700

    Add: test for mergeRecurse function

--- test.js
+++ test.js
@@ -9,3 +9,14 @@ describe('merge function ', function (){
     expect(merge(arr1, arr2)).to.deep.equal([2,3,4,4,5,5]);
   });
 });
+
+var mergeRecurse = require('./mergeRecurse.js');
+
+describe('merge using recursion ', function () {
+  it('merges two array', function () {
+    var arr1 = [2,3,4];
+    var arr2 = [4,5,5];
+    //console.log(merge);
+    expect(merge(arr1, arr2)).to.deep.equal([2,3,4,4,5,5]);
+  });
+});
