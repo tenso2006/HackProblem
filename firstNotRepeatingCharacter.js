12494fe879c89d1d0863f1a3d00fd51703a33d65
Tenzin Sonam <tenso2006@gmail.com>
2017-06-29 12:38:29 -0700

    Refactor: firstNotRepeatedCharacter to be linear

--- firstNotRepeatingCharacter.js
+++ firstNotRepeatingCharacter.js
@@ -2,34 +2,36 @@ function firstNotRepeatingCharacter(s) {
     //declare a obj to hold the unique char count
     var obj = {};
     var arr = [];
-    //var nonRepeat = [];
+    var nonRepeat = [];
 
     //iterate over the string s to build obj to find unique char count
     for(var i=0; i< s.length; i++) {
         if(obj.hasOwnProperty(s[i])) {
             obj[s[i]]++;
-            //nonRepeat.splice(nonRepeat.indexOf(s[i]),1);
+            nonRepeat.splice(nonRepeat.indexOf(s[i]),1);
         } else {
             obj[s[i]] = 1;
-            //nonRepeat.push(s[i]);
+            nonRepeat.push(s[i]);
         }
     }
 
-    //iterate over obj to take out all the key with 1 as count in arr
-    for(var key in obj) {
-        if(obj[key] === 1) {
-            arr.push(key);
-        }
-    }
+    return nonRepeat.length === 0 ? '_' : nonRepeat[0];
 
-    //iterate over the string s again to check if any letter is in arr
-    for(var j=0; j< s.length; j++) {
-        if (arr.includes(s[j])) {
-            return s[j];
-        }
-    }
-    //console.log(nonRepeat);
-    return '_';
+    // //iterate over obj to take out all the key with 1 as count in arr
+    // for(var key in obj) {
+    //     if(obj[key] === 1) {
+    //         arr.push(key);
+    //     }
+    // }
+    // console.log(nonRepeat);
+    // //iterate over the string s again to check if any letter is in arr
+    // for(var j=0; j< s.length; j++) {
+    //     if (arr.includes(s[j])) {
+    //         return s[j];
+    //     }
+    // }
+
+    // return '_';
 }
 
 console.log(firstNotRepeatingCharacter('abcd'));
